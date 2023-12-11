e9a85ebaf3410decd6072548d90b1ce51db67de8
Zachary Szeto <60451103+zachszeto@users.noreply.github.com>
2023-12-10 22:38:57 -0500

    Create 217. Contains Duplicate.cpp

---
+++ C/217. Contains Duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for(int i = 0; i < nums.size(); i++){
+            s.insert(nums[i]);
+        }
+
+        return nums.size() != s.size();
+    }
+};
