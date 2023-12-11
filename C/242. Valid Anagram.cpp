deb3d20fb08f1b89059a8911156799f4332d30fb
Zachary Szeto <60451103+zachszeto@users.noreply.github.com>
2023-12-10 22:42:22 -0500

    Create 242. Valid Anagram.cpp

---
+++ C/242. Valid Anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        //Key: letter, Value: count
+        unordered_map<char, int> sMap;
+        unordered_map<char, int> tMap;
+
+        for(int i = 0; i < s.size(); i++){
+            sMap[s[i]]++;
+            tMap[t[i]]++;
+        }
+
+        for(int i = 0; i < sMap.size(); i++){
+            if(sMap[i] != tMap[i]) return false;
+        }
+
+        return true;
+    }
+};
